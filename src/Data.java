3fddaa880a282134724303299a32cf8172e556ab
JayTe <aloys.augustin@polytechnique.org>
2015-03-12 19:16:16 +0100

    Read input

--- src/Data.java
+++ src/Data.java
@@ -1,9 +1,23 @@
+import java.io.File;
+import java.util.Scanner;
+
 /**
  * Created by aloys on 12/03/15.
  */
 public class Data {
 
+    public static final int DISPO = -1;
+    public static final int INDISPO = -2;
+
     class Serveur {
+        public Serveur() {
+            capacite = 0;
+            taille = 0;
+            groupe = -1;
+            rangee = -1;
+            position = -1;
+        }
+
         int capacite;
         int taille;
         int groupe;
@@ -17,15 +31,43 @@ public class Data {
     int groupes;
     int num_serveurs;
     Serveur[] serveurs;
-    int[][] indispo;
+    int[][] grille;
 
 
     public Data() {
-
     }
 
-    public void read(String filename) {
+    public boolean read(String filename) {
+        File f = new File(filename);
+        try {
+            Scanner sc = new Scanner(f);
+            rangees = sc.nextInt();
+            emplacements = sc.nextInt();
+            num_indispo = sc.nextInt();
+            groupes = sc.nextInt();
+            num_serveurs = sc.nextInt();
+            grille = new int[rangees][emplacements];
+            for (int i = 0; i < rangees; i ++) {
+                for (int j = 0; j < emplacements; j ++) {
+                    grille[i][j] = DISPO;
+                }
+            }
+            for (int i = 0; i < num_indispo; i ++) {
+                int r = sc.nextInt();
+                int e = sc.nextInt();
+                grille[r][e] = INDISPO;
+            }
+            serveurs = new Serveur[num_serveurs];
+            for (int i = 0; i < num_serveurs; i ++) {
+                serveurs[i].taille = sc.nextInt();
+                serveurs[i].capacite = sc.nextInt();
+            }
 
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 
 }

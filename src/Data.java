8ad9736a45a2a2dad905c2ba2d51b08e04cc48ea
JayTe <aloys.augustin@polytechnique.org>
2015-03-12 19:00:44 +0100

    Data structure

---
+++ src/Data.java
@@ -0,0 +1,31 @@
+/**
+ * Created by aloys on 12/03/15.
+ */
+public class Data {
+
+    class Serveur {
+        int capacite;
+        int taille;
+        int groupe;
+        int rangee;
+        int position;
+    }
+
+    int rangees;
+    int emplacements;
+    int num_indispo;
+    int groupes;
+    int num_serveurs;
+    Serveur[] serveurs;
+    int[][] indispo;
+
+
+    public Data() {
+
+    }
+
+    public void read(String filename) {
+
+    }
+
+}
